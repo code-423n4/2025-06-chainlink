8b8d91eb71fc6716b414709098850d11ca550dc9
Alexander Papageorgiou <alex.ppg@protonmail.com>
2025-06-14 11:45:11 +0300

    Code: Inclusion of PoC test file

---
+++ test/PoC.t.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.8.26;
+
+import {BaseTest} from "./BaseTest.t.sol";
+
+contract PoC is BaseTest {
+  function test_submissionValidity() 
+    external
+    // Modifiers of BaseTest for setup
+  {
+    // Proof of Concept code demonstrating the vulnerability
+  }
+}
